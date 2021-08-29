d24b7708354165bef3a513a5bfb63f95d89a759e
itagusu <ir060906@gmail.com>
2021-08-29 08:02:12 +0000

    [finish]5-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
 
   def show
+    @new_book = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
@@ -15,12 +19,16 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
+      @user = current_user
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    unless current_user.id == @book.user.id
+      redirect_to books_path
+    end
   end
 
 
@@ -34,16 +42,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
